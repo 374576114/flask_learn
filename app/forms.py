7da052de6a4515ba216f7e6b5f2d9c258da6d10e
crabo_yang <374576114@qq.com>
2017-10-11 23:15:01 +0800

    add user info and profile 1.change database 2.add Edit

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,13 @@
 #from flask.ext.wtf import Form
 from flask_wtf import FlaskForm
 #from flaskForm import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
 	openid = StringField('openid', validators=[DataRequired()])
 	remember_me = BooleanField('remember_me', default=False)
+
+class EditForm(FlaskForm):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=4, max=140)])
--- app/models.py
+++ app/models.py
@@ -1,13 +1,17 @@
 from app import db
-
-ROLE_USER = 0
-ROLE_ADMIN = 1
+from hashlib import md5
 
 class User(db.Model):
+    '''
+    if we change the table  elements
+    we should './db_migrate.py' to upgrade our database
+    '''
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(120), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     def is_authenticated(self):
         return True
@@ -26,6 +30,11 @@ class User(db.Model):
 
     def __repr___(self):
         return '<User %r>' % (self.nickname)
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + \
+                md5(self.email).hexdigest() + \
+                '?d=mm&s=' + str(size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
